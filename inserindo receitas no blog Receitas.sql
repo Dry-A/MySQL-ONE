d72007a1d52cf2ca23497a08e08aa7a122242659
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-10-22 23:00:07 -0300

    novos exercicios com tabelas aninhadas

---
+++ inserindo receitas no blog Receitas.sql
@@ -0,0 +1,8 @@
+USE db_blogreceitas;
+
+INSERT INTO tb_receitas (
+titulo, ingredientes, modo_preparo) VALUES
+('Torta de Liquidificador', 'Farinha de trigo, ovos, leite, oleo, fermento', 'balsoasjjk sadhjjhdsjdhs sjdhjshja dhbhbhdbsd hdbhbdfh hbdfhfb');
+
+
+SELECT *FROM tb_receitas;
